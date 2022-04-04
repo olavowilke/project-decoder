2369a0836d8452e678fdf87abd5b07c70d7d55ea
Olavo Wilke <olavo.wilke@gmail.com>
2022-04-04 15:08:45 -0300

    creating packages

---
+++ authuser/src/main/java/com/ead/authuser/EadApplication.java
@@ -0,0 +1,13 @@
+package com.ead.authuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EadApplication.class, args);
+    }
+
+}
