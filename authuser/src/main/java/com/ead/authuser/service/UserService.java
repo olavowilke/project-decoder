f016ce2b1eacb7367b0542d20fc97a4ea0fb128b
Olavo Wilke <olavo.wilke@gmail.com>
2022-04-09 10:58:33 -0300

    creating controller layer

--- authuser/src/main/java/com/ead/authuser/service/UserService.java
+++ authuser/src/main/java/com/ead/authuser/service/UserService.java
@@ -1,6 +1,18 @@
 package com.ead.authuser.service;
 
+import com.ead.authuser.model.User;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 public interface UserService {
 
+    List<User> findAll();
+
+    Optional<User> findById(UUID userId);
+
+    ResponseEntity<Object> deleteUser(User userOptional);
 
 }
--- authuser/src/main/java/com/ead/authuser/service/impl/UserServiceImpl.java
+++ authuser/src/main/java/com/ead/authuser/service/impl/UserServiceImpl.java
@@ -1,12 +1,38 @@
 package com.ead.authuser.service.impl;
 
+import com.ead.authuser.model.User;
 import com.ead.authuser.repository.UserRepository;
 import com.ead.authuser.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
 public class UserServiceImpl implements UserService {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Override
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public Optional<User> findById(UUID userId) {
+        return userRepository.findById(userId);
+    }
+
+    @Override
+    public ResponseEntity<Object> deleteUser(User userOptional) {
+        return null;
+    }
+
 
 }
