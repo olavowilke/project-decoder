2a731c1c3be1864a6cb1ecf09890f3a08bcd45b9
Olavo Wilke <olavo.wilke@gmail.com>
2022-04-24 13:31:55 -0300

    API RESTFul para Microservices: Do Básico ao Avançado concluido

--- authuser/src/main/java/com/ead/authuser/AuthuserApplication.java
+++ authuser/src/main/java/com/ead/authuser/AuthuserApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EadApplication {
+public class AuthuserApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(EadApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(AuthuserApplication.class, args);
+	}
 
 }
--- authuser/src/main/java/com/ead/authuser/models/UserModel.java
+++ authuser/src/main/java/com/ead/authuser/models/UserModel.java
@@ -1,66 +1,54 @@
-package com.ead.authuser.model;
+package com.ead.authuser.models;
 
 import com.ead.authuser.enums.UserStatus;
 import com.ead.authuser.enums.UserType;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+import org.springframework.hateoas.RepresentationModel;
 
 import javax.persistence.*;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
 @Entity
-@Getter
-@Setter
-@NoArgsConstructor
-@Table(name = "tb_user")
-public class User implements Serializable {
-
+@Table(name = "TB_USERS")
+public class UserModel extends RepresentationModel<UserModel> implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private UUID uuid;
-
-    @Column(nullable = false, unique = true)
+    private UUID userId;
+    @Column(nullable = false, unique = true, length = 50)
     private String username;
-
-    @Column(nullable = false, unique = true)
+    @Column(nullable = false, unique = true, length = 50)
     private String email;
-
-    @Column(nullable = false)
+    @Column(nullable = false, length = 255)
     @JsonIgnore
     private String password;
-
-    @Column(nullable = false)
+    @Column(nullable = false, length = 150)
     private String fullName;
-
     @Column(nullable = false)
     @Enumerated(EnumType.STRING)
     private UserStatus userStatus;
-
     @Column(nullable = false)
     @Enumerated(EnumType.STRING)
     private UserType userType;
-
-    @Column
+    @Column(length = 20)
     private String phoneNumber;
-
-    @Column
+    @Column(length = 20)
     private String cpf;
-
     @Column
     private String imageUrl;
-
     @Column(nullable = false)
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyy HH:mm:ss")
+    @JsonFormat(shape = JsonFormat.Shape.STRING,pattern = "dd-MM-yyyy HH:mm:ss")
     private LocalDateTime creationDate;
-
     @Column(nullable = false)
+    @JsonFormat(shape = JsonFormat.Shape.STRING,pattern = "dd-MM-yyyy HH:mm:ss")
     private LocalDateTime lastUpdateDate;
 
 }
