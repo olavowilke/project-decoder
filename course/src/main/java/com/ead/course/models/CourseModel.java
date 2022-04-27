141f1e3783f7e21a5cbb20c56724e56a62bd2711
Olavo Wilke <olavo.wilke@gmail.com>
2022-04-26 21:22:51 -0300

    adding notes

--- course/src/main/java/com/ead/course/models/CourseModel.java
+++ course/src/main/java/com/ead/course/models/CourseModel.java
@@ -52,15 +52,31 @@ public class CourseModel implements Serializable {
     relationship.
 
     The fetch = FetchType.LAZY tells Hibernate to lazily load courses for a given module.
-    This means when we retrieve an module from the database, Hibernate won't return the associated courses
-    for that module in the same call. A separate request is made to the database only when the
-    application explicitly asks for module.getCourses().
+    This means when we retrieve a course from the database, Hibernate won't return the associated modules
+    for that course in the same call. A separate request is made to the database only when the
+    application explicitly asks for course.getModules().
 
     The mappedBy = "course" attribute tells us the course table is the owning side of the relationship.
+
+    When creating associated elements, Set is recommended over List, due to Hibernates perfomance
      */
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     @OneToMany(mappedBy = "course", fetch = FetchType.LAZY)
     @Fetch(FetchMode.SUBSELECT)
     private Set<ModuleModel> modules;
+
+    /*
+    FetchMode.SELECT
+        Runs individual sql queries to retrieve each module from the specific course.
+    FetchMode.JOIN
+        Runs a single query to retrieve the course and its modules
+    FetchMode.SUBSELECT
+        Will execute two queries: One that will retrieve the course and another one to retrieve
+        all its modules
+
+    Bidirectional one-to-many and both many-to-one association mappings are fine. But you should avoid unidirectional
+    one-to-many associations in your domain model. Otherwise, Hibernate might create unexpected tables and execute
+    more SQL statements than you expected.
+     */
 
 }
